cae133cc0f2cf09ae47bbb4b3b2410e75b91541d
Marcos-mi <marcosminof22@gmail.com>
2022-10-20 16:35:50 -0300

    agrego urls

--- proyectos.php
+++ proyectos.php
@@ -25,13 +25,13 @@ $pg= "proyectos";
     <!--main container principal-->
     <main class="container">
         <div class="row">
-            <div class="col-12 pt-2 pb-5">
+            <div class="col-12 pt-2">
                 <h1>Proyectos</h1>
             </div>
         </div>
         <div class="row">
             <div class="col-12">
-                <p>Los siguientes son algunos de los trabajos que he realizado:</p>
+                <p>Los siguientes son algunos de los trabajos que he realizado</p>
             </div>
         </div>
         <!--Seccion 1-->
@@ -74,7 +74,7 @@ $pg= "proyectos";
                             <a href="#" class="btn-rojo">Ver online</a>
                         </div>
                         <div class="col-6 link-proyectos">
-                            <a href="https://github.com/Marcos-mi/Paginas-Programas-en-php/tree/main/sistema_ventas">Código fuente</a>
+                            <a href="https://github.com/Marcos-mi/Paginas-Programas-en-php/tree/main/sistema_ventas" target="_blank">Código fuente</a>
                         </div>
                     </div>
                 </div>
@@ -110,7 +110,7 @@ $pg= "proyectos";
                         <h2>Devstagram</h2>
                     </div>
                     <div class="col-12 py-3">
-                        <p>Proyecto Full Stack desarrollado en PHP, Laravel, Javascript,  HTML, CSS,Alpine.Js,Livewire,mvc, MySQL,docker,  gestion de usuarios, módulo de permisos y funcionalidades a fines.</p>
+                        <p>Proyecto Full Stack desarrollado en PHP, Laravel, Javascript,  HTML, CSS,Tailwind CSS, Alpine.Js,Livewire,MVC, MySQL,docker, módulo de permisos y funcionalidades a fines.</p>
                     </div>
                     <div class="row py-5">
                         <div class="col-6">
